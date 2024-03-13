1d7fe9e8beca9d15ca404a4b8c22006cb5ca9fbc
arisene <karuretwaarsene744@gmail.com>
2024-03-13 23:27:23 +0200

    9-12

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,10 @@
-
-
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase_text = ""
+    for wd in str:
+        if "a" <= wd <= "z":
+            uppercase_char = chr(ord(wd) - 32)
+            uppercase_text += uppercase_char
+        else:
+            uppercase_text += wd
+    return(print("{}".format(uppercase_text)))
