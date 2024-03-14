c6f7b39412aa4cde08c88c06d8c7fc6c281c6d78
arisene <karuretwaarsene744@gmail.com>
2024-03-14 22:33:26 +0200

    3-5

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,8 @@ if __name__ == "__main__":
     i = 1
     if num_arg == 0:
         print("0 arguments.")
+    elif num_arg == 1:
+        print("{} argument: ".format(num_arg))
     else:
         print("{} arguments: ".format(num_arg))
     while i < (num_arg + 1):
