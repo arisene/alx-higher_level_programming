010cb4cefb28b866763f5b3008d507cd584c3181
arisene <karuretwaarsene744@gmail.com>
2024-03-16 15:24:48 +0200

    go

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    last_char = my_string[-1]  # Get the last character before removing 'c's
 
     for st in my_string:
-        if st == 'c' or st == 'C':
-            continue
-        else:
+        if not st.lower() == 'c':
             print("{:s}".format(st), end='')
-    return (st)
+
+    return last_char
