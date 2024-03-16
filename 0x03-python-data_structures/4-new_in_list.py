14b554bf14affdf95e38b5ce5511839685505876
arisene <karuretwaarsene744@gmail.com>
2024-03-16 15:04:51 +0200

    slicing

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,8 @@
 def new_in_list(my_list, idx, element):
 
     copied_list = list(my_list)
-    len_list = len(copied_list) - 1
-    if idx < 0:
-        return copied_list
-    elif idx < len_list:
-        copied_list[idx] = element
+    if idx < 0 or idx > len(copied_list) - 1:
         return copied_list
     else:
+        copied_list[idx] = element
         return copied_list
