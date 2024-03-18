0dc97df836c2632cd533908add73124d6616ebb9
arisene <karuretwaarsene744@gmail.com>
2024-03-18 15:42:58 +0200

    10-12

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multiple_two = []
+    if len(my_list) > 0:
+        for i in range(len(my_list)):
+            if my_list[i] % 2 == 0:
+                multiple_two.append(True)
+            else:
+                multiple_two.append(False)
+        return multiple_two
