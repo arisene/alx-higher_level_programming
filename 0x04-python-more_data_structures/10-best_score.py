37d02c9a849c9598c81010f8bcd6322c019d35d9
arisene <karuretwaarsene744@gmail.com>
2024-03-21 11:16:52 +0200

    8-12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_score = 0
+    max_key = None
+    for k, y in a_dictionary.items():
+        if max_key is None or max_score < y:
+            max_score = y
+            max_key = k
 
+    return max_key
