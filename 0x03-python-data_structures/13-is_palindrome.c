f0f5534aa7bdec7e7d90997fda1910ffbe15a7b0
arisene <karuretwaarsene744@gmail.com>
2024-03-31 13:44:48 +0200

    go

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,35 +1,41 @@
 #include "lists.h"
+
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: linked list.
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * is_palindrome - determine if singly linked list is palindrome
+ * @head: pointer to head of singly linked list
+ * Return: 0 if not, 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *p = NULL;
-	int i = 0, size = 0, j = 0, flag = 1;
-	int *a = NULL;
+	listint_t *tmp = *head;
+	unsigned int size = 0, i = 0;
+	int data[10240];
 
-	if (head == NULL || *head == NULL)
-		return (flag);
-	p = *head;
-	while (p->next != NULL)
+	if (head == NULL) /* non-existing list is not */
+		return (0);
+
+	if (*head == NULL) /* empty list is palindrome */
+		return (1);
+
+	while (tmp) /* find size of linked list */
 	{
-		size++;
-		p = p->next;
+		tmp = tmp->next;
+		size += 1;
 	}
-	a = malloc(sizeof(int) * size + 1);
-	p = *head;
-	while (p != NULL)
+	if (size == 1) /* single node list is palindrome */
+		return (1);
+
+	tmp = *head;
+	while (tmp) /* pull node data into array to compare */
 	{
-		a[i] = p->n;
-		i++;
-		p = p->next;
+		data[i++] = tmp->n;
+		tmp = tmp->next;
 	}
-	j = size;
-	for (i = 0; i <= size / 2; i++, j--)
-		if (a[i] != a[j])
-			flag = 0;
-	free(a);
-	return (flag);
+
+	for (i = 0; i <= (size/2); i++)
+	{
+		if (data[i] != data[size - i - 1])
+			return (0);
+	}
+	return (1);
 }
