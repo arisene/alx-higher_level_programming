4b41ad7cee10b3724c09837120dbd36a5b881d3e
arisene <karuretwaarsene744@gmail.com>
2024-03-31 13:16:11 +0200

    go

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
-import dis
-
-
-def print_defined_names(filename):
-
-    with open(filename, 'rb') as file:
-        code = file.read()
-
-    # Disassemble the bytecode
-    instructions = dis.get_instructions(code)
-
-    # Extract and print names
-    names = set()
-    for instr in instructions:
-        if instr.opname == 'LOAD_NAME' and not instr.argval.startswith('__'):
-            names.add(instr.argval)
-
-    # Print names in alphabetical order
-    for name in sorted(names):
-        print(name)
-
-
 if __name__ == "__main__":
-    print_defined_names("hidden_4.pyc")
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
