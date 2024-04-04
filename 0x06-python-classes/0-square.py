4db2f48d6700d467c642424ab095095330401704
arisene <karuretwaarsene744@gmail.com>
2024-04-04 23:50:18 +0200

    class, attributes, methods

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 class Square:
-
-    def __init__(self):
-        """
-
-        Args:
-            side: The lenght of the sides of the square.
-
-        Returns:
-            has no return value
-        """
-
-        return
+    pass
