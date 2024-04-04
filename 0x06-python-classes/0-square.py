6240b2a9f6daca1fbea278f3e4fa7c2ae0a3c0f4
arisene <karuretwaarsene744@gmail.com>
2024-04-04 23:47:01 +0200

    class, attributes, methods

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self):
+        """
+
+        Args:
+            side: The lenght of the sides of the square.
+
+        Returns:
+            has no return value
+        """
+
+        return
