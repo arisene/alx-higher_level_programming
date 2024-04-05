fbedf26565a4fb0ee05d714586edce316c6e6e1b
arisene <karuretwaarsene744@gmail.com>
2024-04-06 00:38:19 +0200

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Based on 0-square.py
+a square with a private instance attribute
+"""
+
+
+class Square:
+    """size in the private instance attribute"""
+    def __init__(self, __size):
+        self.__size = 3
