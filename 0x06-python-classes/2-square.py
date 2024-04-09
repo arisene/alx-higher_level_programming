797c1d01fd3e00020e68bacd0e734f25a8c8b33d
arisene <karuretwaarsene744@gmail.com>
2024-04-09 10:50:18 +0200

    attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-Define  class Square
-args:
-    size(int) the length of the sides
+Module 2
+A class that defines a square
 """
 
 
 class Square:
 
     """
-    initialize class Square
-    attributes:
-        __size a private attribute of size
-
+    Define  class Square
+    args:
+        size(int) the length of the sides
     """
     def __init__(self, size=0):
+
+        """
+        initialize class Square
+        attributes:
+            __size a private attribute of size set default to zero
+        """
 
         if not type(size) is int:
             raise TypeError("size must be an intege")
