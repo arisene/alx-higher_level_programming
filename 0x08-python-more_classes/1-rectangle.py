717331ea08446f9253422c7de6aa3da2b0e8c96f
arisene <karuretwaarsene744@gmail.com>
2024-04-11 17:28:16 +0200

    go hard or go home

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 Module 2 creating a class called rectangle
+with private attributes width and height
 """
 
 
@@ -23,23 +24,14 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """
         the instantiation method of width and height
-        attributes:
-            width of the rectangle set to zero as default
-            height of the rectangle set to zero as default
-        returns:
-            has no return value
-
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
         """
         the getter method of the height
-        attributes: takes no attribute
-        returns:
-            the current value of height
         """
         return self.__height
 
@@ -47,13 +39,6 @@ class Rectangle:
     def height(self, value):
         """
         the setter method of the height
-        attributes:
-            value the new value of the height
-        raises:
-            TypeError in case the value is not an int
-            ValueError in case the value is less than zero
-        returns:
-            has no return value
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -66,9 +51,6 @@ class Rectangle:
     def width(self):
         """
         the getter method of the width
-        attributes: takes no attribute
-        returns:
-            the current value of width
         """
         return self.__width
 
@@ -76,13 +58,6 @@ class Rectangle:
     def width(self, value):
         """
         the setter method of the width
-        attributes:
-            value the new value of the width
-        raises:
-            TypeError in case the value is not an int
-            ValueError in case the value is less than zero
-        returns:
-            has no return value
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
