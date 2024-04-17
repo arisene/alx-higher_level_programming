d124896483cb3f36dcee49e3b4f1a89df78b53ed
arisene <karuretwaarsene744@gmail.com>
2024-04-17 11:36:50 +0200

    do so

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,7 @@
 module 2 Matrix
 """
 
+
 def are_integer(matrix):
     """
     check if the matrix's elements
@@ -12,18 +13,22 @@ def are_integer(matrix):
         for eleh in row:
             if not isinstance(eleh, (int, float)):
                 return False
-            
     return True
+
 
 def equal_rows(matrix):
     """ check rows of the matrix"""
     row_sizes = {len(row) for row in matrix}
     return len(row_sizes) == 1
 
-def matrix_divided(matrix, div):
 
-    if not isinstance(type(matrix), list) or are_integer(matrix) != True or len(matrix[0]) == 1:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+msg = "matrix must be a matrix (list of lists) of integers/floats"
+
+
+def matrix_divided(matrix, div):
+    """ divide the elements of the matrix by div"""
+    if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError(msg)
     elif equal_rows(matrix) != 1:
         raise TypeError("Each row of the matrix must have the same size")
     elif not isinstance(div, (int, float)):
