458d95a1dcbbc7723d4abbbae3019acb9f5e51d2
arisene <karuretwaarsene744@gmail.com>
2024-04-23 00:13:32 +0200

    go

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,12 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module 0 function that returns a list
+of methods and attributes of an object
+"""
+
 
 def lookup(obj):
+    """ returns a list of att & Meth"""
     list_methods = list()
     list_methods.append(dir(obj))
 
