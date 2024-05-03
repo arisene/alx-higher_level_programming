a7a8f3a67ee95895cefb02b86b194f42071533a0
arisene <karuretwaarsene744@gmail.com>
2024-05-03 12:37:39 +0200

    task 8-9

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -109,7 +109,7 @@ class Rectangle:
         """
         Rectangle.number_of_instances -= 1
         print('Bye rectangle...')
-
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         rect_1 = rect_1
         rect_2 = rect_2
