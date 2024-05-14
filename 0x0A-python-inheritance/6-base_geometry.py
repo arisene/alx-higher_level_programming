ad7f7da7d54c95b1cd5fd9f0e44d9597334252b3
arisene <karuretwaarsene744@gmail.com>
2024-05-14 20:44:47 +0200

    go

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+Module six
+"""
+
 
 class BaseGeometry():
+    """
+    class Base-geometry
+    raise exception for area
+    """
     def area(self):
+        """
+        area method
+        """
         raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Module seven
+"""
+
 
 class BaseGeometry():
+    """
+    BaseGeometry
+    """
     def area(self):
         raise Exception("area() is not implemented")
 
