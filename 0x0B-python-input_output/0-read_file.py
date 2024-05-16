3cf9b1e0d8a5440cb77c9a5d7ecd46c2de2a11c9
arisene <karuretwaarsene744@gmail.com>
2024-05-16 16:18:25 +0200

    file I/O

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+Module 0
+"""
 
 
 def read_file(filename=""):
+    """
+    Reads and print to stdout the content
+    of the text-file
+    """
     with open(filename, mode="r", encoding="utf-8") as myfile:
         fp = myfile.read()
     print(fp, end="")
