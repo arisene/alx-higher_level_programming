98c823bfa5fdd0be3a90f2bcf1aff236ab67a422
arisene <karuretwaarsene744@gmail.com>
2024-08-03 22:25:43 +0200

    11 is suspesious

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,10 @@
 const { argv } = require('node:process');
 
 const numList = [];
-if (isNaN(argv[2]) || argv.length <= 2) {
+if (isNaN(argv[2]) || argv.length < 2) {
   console.log(0);
+}else if(isNaN(argv[3])){
+    console.log(0);
 } else {
   for (let i = 2; i <= argv.length - 1; i++) {
     numList.push(argv[i]);
